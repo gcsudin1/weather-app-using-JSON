e0abe0a499c97f07d362d8bd09557326ebb3f356
Phanindranath Thota <phanindranath.wg113@gmail.com>
2019-08-06 02:24:21 -0500

    codes added

--- weather app using JSON/ViewController.swift
+++ weather app using JSON/ViewController.swift
@@ -7,14 +7,47 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var myTextField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    @IBAction func checkWeather(_ sender: Any) {
+        let cityNameURL = "http://api.openweathermap.org/data/2.5/weather?q=" + myTextField.text! + ",us&appid=88aaee13d46787a563b0888b30e4c419"
+        let urlString = cityNameURL.replacingOccurrences(of: " ", with: "%20")
+        let url = URL(string: urlString)
+        let task = URLSession.shared.dataTask(with: url!){(data,response,error) in
+            if error == nil{
+                if let unwrappedData = data{
+                    let dataString = String(data: unwrappedData, encoding: .utf8)
+                    if dataString!.contains("city not found"){
+                        DispatchQueue.main.async {
+                            self.textView.text = "Invalid city name"
+                        }
+                    }else{
+                        do{
+                            let jsonResult = try JSONSerialization.jsonObject(with: unwrappedData, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary
+                            let weather = jsonResult?["weather"] as? NSArray
+                            let weatherItem = weather?[0] as? NSDictionary
+                            let description = weatherItem?["description"] as! String
+                            DispatchQueue.main.async {
+                                self.textView.text = description
+                            }
+                        } catch{
+                            self.textView.text = "Invalid City Name"
+                        }
+                    }
+                }
+            }
+        }
+        task.resume()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let img = UIImage(named:"weather.jpg")
+        view.backgroundColor = UIColor(patternImage: img!)
     }
-
-
 }
+
 
